7c52dc09395dcc58f111b6b6bed98454b4d4df3e
deksden <deksden@deksden.com>
2016-06-30 21:01:57 +0600

    More ES6

--- controllers/contact.js
+++ controllers/contact.js
@@ -1,3 +1,4 @@
+'use strict';
 const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   service: 'SendGrid',
--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,4 @@
+'use strict';
 const async = require('async');
 const crypto = require('crypto');
 const nodemailer = require('nodemailer');
